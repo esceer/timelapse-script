ed60a8eb85b597c7bc433d0aa4aa5bd471d14413
Roland Kutas <33330748+esceer@users.noreply.github.com>
2021-05-29 08:20:57 +0200

    Change api uri

--- src/api/api.py
+++ src/api/api.py
@@ -10,12 +10,12 @@ from utils.timelapse import TimelapseEngine
 def make_wsgi_app(config: Config, cron_tab: CronTab):
     app = Bottle()
 
-    @app.get('/timelapse/schedule')
+    @app.get('/timelapse/start')
     def schedule_timelapse_handler():
         cron_tab.setup_timelapse_cron_jobs()
         return 'Scheduled.'
 
-    @app.get('/timelapse/unschedule')
+    @app.get('/timelapse/stop')
     def unschedule_timelapse_handler():
         cron_tab.truncate_cron_jobs()
         return 'Unscheduled.'
